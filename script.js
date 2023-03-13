6bffa43be160c16bc1ea4ccbd04cdb4cb63ef588
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-03-13 13:42:54 +0700

    Create a day of the week translator Completed

---
+++ script.js
@@ -0,0 +1,42 @@
+// Variable dayNumber
+// 0,1,2,3,4,5,6
+// S,M,T,W,T,F,S
+const dayNumber = 0;
+// Variable day
+let day;
+// Switch by dayNumver
+switch (dayNumber) {
+    // if dayNumber === 0
+    case 0:
+        day = "Sunday";
+        break;
+    // if dayNumber === 1
+    case 1:
+        day = "Monday";
+        break;
+    // if dayNumber === 2
+    case 2:
+        day = "Tuesday";
+        break;
+    // if dayNumber === 3
+    case 3:
+        day = "Wednesday";
+        break;
+    // if dayNumber === 4
+    case 4:
+        day = "Thursday";
+        break;
+    // if dayNumber === 5
+    case 5:
+        day = "Friday";
+        break;
+    // if dayNumber === 6
+    case 6:
+        day = "Saturday";
+        break;
+    // if dayNumber > 6
+    default:
+        day = "Invalid day number";
+        break;
+}
+console.log(day);
